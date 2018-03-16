d4ec7134caec8a1d7b757fa15c3bf749bdb74d2c
K <z@kwi.li>
2018-03-16 01:10:52 -0400

    Cache builds

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
+            cacheDirectory: true,
             presets: ['@babel/preset-env'],
             plugins: [
               require('@babel/plugin-proposal-object-rest-spread'),
