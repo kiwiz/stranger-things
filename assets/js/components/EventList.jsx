d4415a60abc586d83ba49e251d12328e25c1665c
K <z@kwi.li>
2018-03-25 10:25:34 -0400

    Fix import statement

--- assets/js/components/EventList.jsx
+++ assets/js/components/EventList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Columns, Column } from './spectre'
+import Columns from './spectre/Columns'
+import Column from './spectre/Column'
 import Event from './Event'
 
 const EventList = ({events}) => {
